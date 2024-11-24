2e46dfb789127ca2c3c7488db4a6136b6c19d755
AdityaMittal08 <mittaladitya0801@gmail.com>
2024-11-24 16:08:35 +0530

    Update Factorial of a number.py

--- Factorial of a number.py
+++ Factorial of a number.py
@@ -2,6 +2,7 @@ ask_user = int(input("Enter the number you want factorial of: "))
 def factorial(n):
     if n == 0 or n == 1:
         return 1
+    # Recursive case
     else:
         return n*factorial(n-1)
     
